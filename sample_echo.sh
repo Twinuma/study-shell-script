2ac51602ca452c1720f99c2da723e91255e2cae8
takayuki niinuma <niinuma.takayuki@gmail.com>
2013-07-07 15:10:19 +0900

    add printf

--- sample_echo.sh
+++ sample_echo.sh
@@ -8,3 +8,12 @@ echo "${s}"
 echo $s$s
 echo "$s $s"
 echo '$3'
+
+x=5
+s="taguchi"
+
+printf "i have %d apples\n" $x
+printf "my name is %s\n" $s
+printf "[%5d]\n" $x
+printf "[%-5d]\n" $x
+printf "[%05d]\n" $x
